b011946f656f0948f89f89a895520f9da865408e
김동현 <52624315+ds5105119@users.noreply.github.com>
2024-08-12 22:36:00 +0900

    병합이 이상하게 된 점을 수정하였습니다./

--- shop/views.py
+++ shop/views.py
@@ -4,11 +4,10 @@ from django.core.paginator import Paginator
 from django.http import HttpResponseForbidden
 
 from django.contrib.auth.models import User
-
 from django.contrib.auth.decorators import login_required
 
 from shop.forms import OrderForm
-from shop.models import Cart, Category,  Product
+from shop.models import Cart, Category, Product
 
 
 def user_verification(func):
@@ -21,11 +20,6 @@ def user_verification(func):
 
         return func(request, *args, **kwargs)
     return wrap
-
-from shop.models import Cart, Category, Product
-from django.contrib.auth.models import User
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 
 
 def index(request):
@@ -82,41 +76,6 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'shop/product_detail.html', context)
-  
-    category = get_object_or_404(Category, pk=category_id)
-    products = Product.objects.filter(category=category).order_by("pub_date")
-    lank_products = Product.objects.filter(category=category).order_by("-hit")[:4]
-    paginator = Paginator(products, 5)
-    page = request.GET.get("page")
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        products = paginator.page(1)
-    except EmptyPage:
-        products = paginator.page(paginator.num_pages)
-    context = {
-        "lank_products": lank_products,
-        "products": products,
-        "category": category,
-        "categories": categories,
-    }
-    return render(request, "shop/category.html", context)
-
-
-def product_detail(request, pk):
-    categories = Category.objects.all()
-    product = get_object_or_404(Product, pk=pk)
-    category = get_object_or_404(Category, pk=product.category.pk)
-    Product.objects.filter(pk=pk).update(hit=F("hit") + 1)
-    quantity_list = list(range(1, product.quantity))
-    context = {
-        "quantity_list": quantity_list,
-        "product": product,
-        "category": category,
-        "categories": categories,
-    }
-    return render(request, "shop/product_detail.html", context)
-
 
 
 @login_required
@@ -137,25 +96,6 @@ def view_cart(request, pk):
 
     return render(request, 'shop/cart.html', context)
 
-    user = get_object_or_404(User, pk=pk)
-    cart_items = Cart.objects.filter(user=user)
-    paginator = Paginator(cart_items, 10)
-    page = request.GET.get("page")
-    try:
-        cart_items = paginator.page(page)
-    except PageNotAnInteger:
-        cart_items = paginator.page(1)
-    except EmptyPage:
-        cart_items = paginator.page(paginator.num_pages)
-    context = {"user": user, "cart": cart_items, "categories": categories}
-    return render(request, "shop/cart.html", context)
-
-
-@login_required
-def delete_cart(request, pk):  # cart 내에서 상품을 지우는 함수
-    user = request.user
-    cart_items = Cart.objects.filter(user=user)
-    quantity = 0
 
 @login_required
 def delete_cart(request, pk):
@@ -173,22 +113,6 @@ def delete_cart(request, pk):
 @login_required
 def add_to_cart(request, pk):
     if request.method == "POST":
-        pk = int(request.POST.get("product"))
-        product = get_object_or_404(Product, pk=pk)
-        for item in cart_items:
-            if item.products == product:
-                quantity = item.quantity
-
-        if quantity > 0:
-            Cart.objects.filter(user=user, products=product).delete()
-            return redirect("shop:cart", user.pk)
-
-
-@login_required
-def add_to_cart(request, product_id):  # 장바구니에 상품 추가
-    if request.method == "POST":
-        quantity = int(request.POST.get("quantity"))
-        product = get_object_or_404(Product, pk=product_id)
         user = request.user
         product = Product.objects.get(pk=pk)
         quantity = int(request.POST.get("quantity"))
@@ -197,9 +121,6 @@ def add_to_cart(request, product_id):  # 장바구니에 상품 추가
         if cart_item:
             cart_item.quantity = min(cart_item.quantity + quantity, product.quantity)
             cart_item.save()
-        cart_item = Cart.objects.filter(user=user, products=product)
-        if cart_item.exists():
-            cart_item.update(quantity=min(F("quantity") + quantity, product.quantity))
         else:
             Cart.objects.create(user=user, products=product, quantity=quantity)
 
