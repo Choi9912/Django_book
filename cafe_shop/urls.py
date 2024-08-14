f2eb1ae888bfafe06cbc5d6b54425f6ef7af535a
NamMinWoo91 <sksfortm21@gmail.com>
2024-08-14 22:33:21 +0900

    teat

--- cafe_shop/urls.py
+++ cafe_shop/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
@@ -21,12 +22,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', shop_views.index, name='root'),
-    path('shop/', include('shop.urls')),
-    path('accounts/', include('accounts.urls')),
-
+    path("admin/", admin.site.urls),
+    path("", shop_views.index, name="root"),
+    path("shop/", include("shop.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+##a
