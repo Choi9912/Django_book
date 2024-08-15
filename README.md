99dca1460a02a891ad54af5ee35f7c5cbbf8999a
Your Name <you@example.com>
2024-08-15 15:53:05 +0900

    readme 화면

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+## 화면 구성 📺
 
+| 메인 페이지 | 상품 목록 |
+| --- | --- |
+| ![main](https://github.com/user-attachments/assets/2b09c5c7-e48d-4c3f-8912-b5ed5aa8ab3f) | ![category](https://github.com/user-attachments/assets/4dbed2a4-531b-480a-9dd4-6f2ee0b305fd) |
+
+| 상품 | 장바구니 | 결제기능 |
+| --- | --- | --- |
+| ![product_detail](https://github.com/user-attachments/assets/0f3132c6-65a1-42dd-abd2-22bc4978df85) | ![cart](https://github.com/user-attachments/assets/b406fe48-9779-454f-b3fd-7473637691e4) | ![pay](https://github.com/user-attachments/assets/9ee50f58-58ac-4e72-898a-90984ea1e0bb) |
+|
--- shop/views.py
+++ shop/views.py
@@ -10,7 +10,6 @@ from shop.forms import OrderForm
 from shop.models import Cart, Category, Product
 
 
-# 아직 사용하지 않는 부분입니다.
 def user_verification(func):
     def wrap(request, *args, **kwargs):
         session_user = User.objects.get(pk=kwargs['pk'])
@@ -45,13 +44,9 @@ def show_category(request, category_id):
     sorted_products = products.order_by('pub_date')
     ranked_products = products.order_by('-hit')[:4]
     
-    try:
-        page = int(request.GET.get('page', 1))
-    except ValueError:
-        page = 1
-    
+    page = int(request.GET.get('page', 1))
     paginator = Paginator(sorted_products, 8)
-    products = paginator.get_page(page)
+    products = paginator.page(page)
 
     context = {
         "categories": categories,
@@ -88,13 +83,9 @@ def view_cart(request, pk):
     cart_list = Cart.objects.filter(user=user)
     item_price_sum = sum(map(lambda item: item.quantity * item.products.price, cart_list))
 
-    try:
-        page = int(request.GET.get('page', 1))
-    except ValueError:
-        page = 1
-    
+    page = int(request.GET.get('page', 1))
     paginator = Paginator(cart_list, 10)
-    cart = paginator.get_page(page)
+    cart = paginator.page(page)
 
     context = {
         "user": user,
@@ -172,3 +163,5 @@ def pay(request, pk):
         }
 
         return render(request, 'shop/order_pay.html', context)
+    
+    
