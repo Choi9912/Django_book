86351ffca559516aea1c955bd1e47194c7788b27
김동현 <52624315+ds5105119@users.noreply.github.com>
2024-09-05 23:13:38 +0900

    Update settings.py

--- cafe_shop/settings.py
+++ cafe_shop/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
     "sorl.thumbnail",
-    "django.contrib.sites",
 ]
 
 MIDDLEWARE = [
